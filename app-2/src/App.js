24d94468695a3f123218a9b21bbef75bb9d43835
Adam Smelly <adam5689@msn.com>
2018-09-06 20:40:11 -0600

    done app2

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,18 +1,25 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      array: ["I", "am", "writing", "an", "Array"]
+    };
+  }
   render() {
     return (
       <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <h2>
+          <ul>{this.state.array[0]}</ul>
+          <ul>{this.state.array[1]}</ul>
+          <ul>{this.state.array[2]}</ul>
+          <ul>{this.state.array[3]}</ul>
+          <ul>{this.state.array[4]}</ul>
+        </h2>
       </div>
     );
   }
